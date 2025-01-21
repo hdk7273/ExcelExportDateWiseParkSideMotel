db72a621cf81d888009955503e61945f9a5e8c54
hdk7273 <hdk7273@gmail.com>
2025-01-20 20:54:02 -0500

    initial commit

---
+++ ExcelAutoGenerator/Program.cs
@@ -0,0 +1,130 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using ExcelAutoGenerator;
+using System.Collections.Generic;
+using NPOI.HSSF.Util;
+using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+using NPOI.XSSF.UserModel;
+using NPOI.HSSF.UserModel;
+using System.Drawing;
+using NPOI.XWPF.UserModel;
+using NPOI.SS.Formula.Functions;
+using Microsoft.AspNetCore.Routing;
+using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("Application Started!");
+string fromDate = string.Empty;//09/01/2023";
+string toDate = string.Empty;//"09/10/2023";
+DateTime fromDateT = DateTime.Now;
+DateTime toDateT = DateTime.Now;
+string OutpathFolder = @"C:\\Users\\Public\\Downloads\\ExcelGenerator\\" + fromDateT.ToString("MM") + "_" + fromDateT.ToString("YYYY");
+Console.WriteLine("Enter from date(MM/DD/YYYY):");
+fromDate = Console.ReadLine();
+Console.WriteLine("Enter to date(MM/DD/YYYY):");
+toDate = Console.ReadLine();
+fromDateT = Convert.ToDateTime(fromDate);
+toDateT = Convert.ToDateTime(toDate);
+if (!Directory.Exists(OutpathFolder))
+{
+    Directory.CreateDirectory(OutpathFolder);
+}
+int dys = 1;
+string inputPathFile = "Readychange.xls";
+string OutpathFile = OutpathFolder + "\\" + fromDateT.ToString("MM") + "_" + fromDateT.ToString("yyyy") + ".xls";
+//File.Copy(inputPathFile, OutpathFile, true);
+using (var fs = new FileStream(OutpathFile, FileMode.Create, FileAccess.Write))
+{
+    IWorkbook workbook = new XSSFWorkbook();
+    foreach (DateTime day in Helper.EachDay(fromDateT, toDateT))
+    {
+        ISheet sheet1 = workbook.CreateSheet(day.ToString("dd MMM"));
+        sheet1.SetColumnWidth(0, 2000);
+        sheet1.SetColumnWidth(1, 25000);
+
+        
+        IRow row;
+        int Room = 101;
+        int middleCounter = 0;
+        for (int i = 0; i < 43; i++)
+        {
+            //style
+            XSSFFont defaultFont = (XSSFFont)workbook.CreateFont();
+            defaultFont.FontHeightInPoints = (short)12;
+            defaultFont.FontName = "Arial";
+            defaultFont.Color = IndexedColors.Black.Index;
+            defaultFont.IsBold = true;
+            XSSFCellStyle firstRowStyle = (XSSFCellStyle)workbook.CreateCellStyle();
+            firstRowStyle.SetFont(defaultFont);
+            firstRowStyle.Alignment = HorizontalAlignment.Center;
+            firstRowStyle.VerticalAlignment = VerticalAlignment.Center;
+            firstRowStyle.WrapText = true;
+            // create bordered cell style
+
+            if (i == 0)
+            {
+                row = sheet1.CreateRow(i);
+                row.Height = 320;
+                row.CreateCell(0).SetCellValue("Room");
+                row.CreateCell(1).SetCellValue(day.ToString("dd MMMM yyyy dddd"));
+                firstRowStyle.BorderLeft = NPOI.SS.UserModel.BorderStyle.Thin;
+                firstRowStyle.BorderTop = NPOI.SS.UserModel.BorderStyle.Thin;
+                firstRowStyle.BorderRight = NPOI.SS.UserModel.BorderStyle.Thin;
+                firstRowStyle.BorderBottom = NPOI.SS.UserModel.BorderStyle.Thin;
+
+                row.GetCell(0).CellStyle = firstRowStyle;
+                row.GetCell(1).CellStyle = firstRowStyle;
+            }
+            else
+            {
+                row = sheet1.CreateRow(i);
+                //row.RowStyle = firstRowStyle;
+                if (i > 1 && i % 3 == 0)
+                {
+                    CellRangeAddress cellMerge = new CellRangeAddress(i-2, i, 0, 0);
+                    sheet1.AddMergedRegion(cellMerge);
+                    Room = Room + 1;
+                }
+                row.Height = 335;
+                row.CreateCell(0).SetCellValue(Room.ToString());
+                row.CreateCell(1).SetCellValue("");
+                row.GetCell(0).CellStyle = firstRowStyle;
+                if (middleCounter == 0)
+                {
+                    firstRowStyle.BorderLeft = NPOI.SS.UserModel.BorderStyle.Thin;
+                    firstRowStyle.BorderTop = NPOI.SS.UserModel.BorderStyle.Thin;
+                    firstRowStyle.BorderRight = NPOI.SS.UserModel.BorderStyle.Thin;
+                    firstRowStyle.BorderBottom = NPOI.SS.UserModel.BorderStyle.Dotted;
+                    row.GetCell(1).CellStyle = firstRowStyle;
+                    middleCounter++;
+                }
+                else if (middleCounter == 1)
+                {
+                    middleCounter++;
+                    firstRowStyle.BorderLeft = NPOI.SS.UserModel.BorderStyle.Thin;
+                    firstRowStyle.BorderRight = NPOI.SS.UserModel.BorderStyle.Thin;
+                    firstRowStyle.BorderTop = NPOI.SS.UserModel.BorderStyle.Dotted;
+                    firstRowStyle.BorderBottom = NPOI.SS.UserModel.BorderStyle.Dotted;
+                    row.GetCell(1).CellStyle = firstRowStyle;
+                }
+                else if (middleCounter > 1)
+                {
+                    middleCounter = 0;
+                    firstRowStyle.BorderLeft = NPOI.SS.UserModel.BorderStyle.Thin;
+                    firstRowStyle.BorderTop = NPOI.SS.UserModel.BorderStyle.Dotted;
+                    firstRowStyle.BorderRight = NPOI.SS.UserModel.BorderStyle.Thin;
+                    firstRowStyle.BorderBottom = NPOI.SS.UserModel.BorderStyle.Thin;
+
+                    row.GetCell(1).CellStyle = firstRowStyle;
+                }
+            }
+        }
+        Console.WriteLine("Day added :" + dys +"/" + Helper.EachDay(fromDateT, toDateT).Count());
+        dys++;
+    }
+    workbook.Write(fs);
+}
+Console.WriteLine("Excel Generated Successfully!");
+Console.ReadLine();
+Environment.Exit(0);
---
+++ ExcelAutoGenerator/Readme.txt
@@ -0,0 +1,13 @@
+This is simple xls generator for Parkside motel.
+
+
+
+To generate file:
+1. This solution has file in it ReadyChange.xls used for template and generate output file at:
+C:\Users\Public\Downloads\ExcelGenerator\01_YYYY
+2. After file got generated for print need to upload on Google Drive:
+https://drive.google.com/drive/folders/19rqcHtFcHw0wR2N3Crpx74LibQ6bmGfs?usp=drive_link
+3. Open in Google Sheet online and generate pdf to print
+4. while setting up file we need to set fit to page and Top 0.357" Left/Right 0.432" Bottom 0.507"
+5. then generate pdf or print directly
+ 
